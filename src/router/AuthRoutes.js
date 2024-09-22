7556d3668edc677db8075fa1e1a879597fcdff88
Genta-arya <mgentaarya@gmail.com>
2024-09-22 09:18:23 +0700

    fix multerss

--- src/router/AuthRoutes.js
+++ src/router/AuthRoutes.js
@@ -1,9 +1,7 @@
 import express from "express";
 import { checkLogin, handleLogin, handleLogout, handleRegister } from "../controller/Authentikasi/AuthController.js";
 
-
 const AuthRouter = express.Router();
-
 AuthRouter.post("/register" , handleRegister)
 AuthRouter.post("/login" , handleLogin)
 AuthRouter.post("/user" , checkLogin)
